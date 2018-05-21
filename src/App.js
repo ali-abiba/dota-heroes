37dc7c73d4999765b34dd329f5ab9e6ed22e9434
Ali Abiba <abibaalih@gmail.com>
2018-05-21 00:56:19 -0400

    Try to fix mobile background

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import HomeComponent from "./home/home.component";
 import Background from './assets/home.jpg';
 import HeroPopularity from "./heroes/hero.popularity/hero.popularity";
 
+
+var styles = {
+    background: `url(${Background}) no-repeat center center fixed`,
+};
+
 class App extends Component {
   render() {
     return (
-        <div className="app-home">
+        <div style={styles} class="app-home">
             <HomeComponent></HomeComponent>
             <HeroPopularity></HeroPopularity>
         </div>
